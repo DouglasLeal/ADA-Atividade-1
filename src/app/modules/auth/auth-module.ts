6a9a31e57db843509bf70f49b83c88c607baeb62
Douglas Leal <douglas.leal@live.com>
2025-08-16 18:06:49 -0300

    Tela cadastro criada

--- src/app/modules/auth/auth-module.ts
+++ src/app/modules/auth/auth-module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing-module';
 import { Login } from './login/login';
+import { Register } from './register/register';
 
 
 @NgModule({
   declarations: [
-    Login
+    Login,
+    Register
   ],
   imports: [
     CommonModule,
