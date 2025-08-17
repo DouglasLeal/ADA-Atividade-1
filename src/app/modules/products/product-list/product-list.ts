24aa8e8188b4a04333e7354f8367de569127995f
Douglas Leal <douglas.leal@live.com>
2025-08-17 11:22:53 -0300

    Ajuste no css do ProductList e ProductListItem

--- src/app/modules/products/product-list/product-list.ts
+++ src/app/modules/products/product-list/product-list.ts
@@ -11,11 +11,17 @@ import { ProductService } from '../../../services/product-service';
 export class ProductList implements OnInit {
   public products: Product[] = [];
 
+  public loadingProducts: boolean = false;
+
+
   constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
+    this.loadingProducts = true;
+
     this.productService.get().subscribe(result => {
-      this.products = result.products;   
+      this.products = result.products;
+      this.loadingProducts = false;   
     })
   }
 }
